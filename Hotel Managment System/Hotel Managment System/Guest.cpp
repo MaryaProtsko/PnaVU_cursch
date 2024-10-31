0a52cb9fdab9f21f96270ec9f82ed1484eba8034
Shweedes <metior22848@mail.ru>
2024-10-31 12:48:11 +0300

    Delete Hotel Managment System directory

--- Hotel Managment System/Hotel Managment System/Guest.cpp
+++
@@ -1,9 +0,0 @@
-#include "Guest.h"
-
-// Реализация конструктора
-Guest::Guest(const std::string& name, int age) : name(name), age(age) {}
-
-// Реализация метода getName()
-std::string Guest::getName() const {
-    return name;
-}
